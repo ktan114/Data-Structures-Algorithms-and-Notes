3a1370db461eb9aa7136ea3307add2b590148cf1
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-06-26 19:58:10 -0400

    Completed bubbleSort

--- Algorithms/Bubble-Sort/index.js
+++ Algorithms/Bubble-Sort/index.js
@@ -7,6 +7,17 @@ Testing: `jest Bubble-Sort/test.js`
 */
 
 function bubbleSort(arr) {
+    // iterate through the array
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = bubbleSort;

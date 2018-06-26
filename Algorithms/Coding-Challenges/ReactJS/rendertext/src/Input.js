12a001e3c0e6bbbb0147c43696c62d6867e715a2
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-06-25 20:01:40 -0400

    Created state, input field, input text function, clear state function, render the input state to the screen

--- Algorithms/Coding-Challenges/ReactJS/rendertext/src/Input.js
+++ Algorithms/Coding-Challenges/ReactJS/rendertext/src/Input.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "", 
+    }
+  }
+
+  handleClear = (e) => {
+    this.setState({ input: "" });
+  }
+
+  inputText = (e) => {
+    e.preventDefault();
+    this.setState({ input: e.target.value })
+  }
+
   render() { 
     return ( 
       <div>
         <h1> User Input </h1>
-        {/* Your Code Goes Here */}
+        <input 
+        type = "text" 
+        name = "input" 
+        value = { this.state.input }
+        placeholder = "Render Some Text"
+        onChange = {this.inputText}
+        />
+        <button onClick = { this.handleClear }>Clear</button>
+        <p> { this.state.input } </p>
       </div>
     )
   }
